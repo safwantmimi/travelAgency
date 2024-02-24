1deca425433f4a05e87d18996c9f4b843b8f6dcb
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2024-02-24 17:12:23 +0100

    adding content to the countries component

--- src/views/countries.js
+++ src/views/countries.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from 'react';
+import "../styles/countries.css";
+
+const countryList = [
+  "United Kingdom",
+  "Czech Republic",
+  "Finland",
+  "Norway",
+  "Switzerland",
+  "Ireland",
+  "Italy",
+  "France",
+  "Spain",
+  "Greece",
+  "Turkey",
+  "Germany",
+  "United States"
+];
+
+const pictures = [
+  require("../images/uk.jpg"),
+  require("../images/czech.jpg"),
+  require("../images/finland.jpg"),
+  require("../images/norway.jpg"),
+  require("../images/switzerland.jpg"),
+  require("../images/ireland.jpg"),
+  require("../images/italy.jpg"),
+  require("../images/france.jpg"),
+  require("../images/spain.jpg"),
+  require("../images/greece.jpg"),
+  require("../images/turkey.jpg"),
+  require("../images/germany.jpg"),
+  require("../images/usa.jpg")
+];
 
 export default function Countries() {
   return (
-    <div className="countriesList row p-0 m-0">
-        <h1><span className='specialText'>Countries </span>travel to </h1>
+    <div className="countriesList row p-2 m-0 d-flex gap-2 justify-content-center">
+      <h1><span className='specialText'>Countries </span>travel to </h1>
+      {countryList.map((country, index) => (
+        <div key={index} className="country d-flex p-2 gap-2 align-items-center col-lg-3">
+          <div className="flag" style={{ backgroundImage: `url(${pictures[index]})` }}></div>
+          <div className="countryName fs-5 pe-2">
+            {country}
+          </div>
+        </div>
+      ))}
     </div>
-
-    )
+  );
 }
