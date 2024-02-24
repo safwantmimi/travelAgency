8419e6ba074ad2e99044ed8aa6232ad5b20b8d3e
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2024-02-24 16:26:05 +0100

    creating the countries component

---
+++ src/views/countries.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Countries() {
+  return (
+    <div className="countriesList row p-0 m-0">
+        <h1><span className='specialText'>Countries </span>travel to </h1>
+    </div>
+
+    )
+}
