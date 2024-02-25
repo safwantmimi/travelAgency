bc03b4f16c6b87d6fa902892ea70251fc174c84f
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2024-02-25 13:25:39 +0100

    adding price and ratings to the tours component

--- src/views/tours.js
+++ src/views/tours.js
@@ -1,9 +1,58 @@
-import React from 'react'
-import "../styles/tours.css"
+import React, { useState } from 'react';
+import "../styles/tours.css";
+import { Rating } from '@mui/material/';
+import { BiExpandAlt } from "react-icons/bi";
+
+const pictures = [
+  require("../images/uk.jpg"),
+  require("../images/czech.jpg"),
+  require("../images/finland.jpg"),
+  require("../images/norway.jpg"),
+  require("../images/switzerland.jpg"),
+  require("../images/ireland.jpg"),
+  require("../images/italy.jpg"),
+  require("../images/france.jpg"),
+  require("../images/spain.jpg"),
+  require("../images/greece.jpg"),
+  require("../images/turkey.jpg"),
+  require("../images/germany.jpg"),
+  require("../images/usa.jpg")
+];
+const ratings = [4,3.5,2,5,1,3.5,4,2,4,3,2,1,5];
+const prices = [27,38,24,32,155,201,75,100,45,78,28,32,50];
+
 export default function Tours() {
+
   return (
-    <div className="tours">
-        <h1>Available <span className="specialText">Tours</span></h1>
+    <div className="tours p-0 m-0">
+      <h1>Available <span className="specialText">Tours</span></h1>
+      <div className="row  m-0 p-0 toursList d-flex gap-2 justify-content-center">
+        {pictures.map((picture, index) => (
+          <div className="card m-0 p-0" style={{ width: "18rem" }} key={index}>
+            <img src={picture} className="card-img-top" alt={`Tour ${index + 1}`} />
+            <div className="card-body">
+              <h5 className="card-title tourDuration fw-normal">7 days </h5>
+              <p className="card-text tourDescription fw-medium">Country Here</p>
+             <div className="container-fluid d-flex gap-2">
+              <Rating
+                name={`rating-${index}`}
+                value={ratings[index]}
+                precision={0.5}
+                readOnly
+                className='rating'
+              />
+              <p className="ratingValue fw-medium">{ratings[index]}</p>
+             </div>
+              
+              
+              <div className="container-fluid d-flex justify-content-between align-items-baseline">
+                <p>From :<span className='tourPrice'>${prices[index]}</span></p>
+                <a href="#" className="btn btn-primary s-flex jusify-content-end viewTourButton m-1"> View Details <BiExpandAlt className='fs-5'/></a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
