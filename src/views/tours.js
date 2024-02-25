a86b5ae53ca2808430fe591b6e51f61039ad5d71
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2024-02-25 11:25:22 +0100

    creating the tours component

---
+++ src/views/tours.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../styles/tours.css"
+export default function Tours() {
+  return (
+    <div className="tours">
+        <h1>Available <span className="specialText">Tours</span></h1>
+    </div>
+  )
+}
