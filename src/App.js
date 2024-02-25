9d1d5a82046b893ad138c76a12d175fa33ce0697
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2024-02-25 11:26:05 +0100

    fixing the background image for the avatar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import AuthRouters from "./routes/authRouters/authRouters"
-import CountriesRouter from './routes/countriesRoute/countriesRouter';
+import ToursRouter from "./routes/toursRouter/toursRouter"
+import CountriesRouter from "./routes/countriesRoute/countriesRouter"
 function App() {
   return (
    <BrowserRouter>
    <AuthRouters></AuthRouters>
    <CountriesRouter></CountriesRouter>
+   <ToursRouter></ToursRouter>
    </BrowserRouter>
   );
 }
--- src/views/login.js
+++ src/views/login.js
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/login.css'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/login.css';
+import userImage from "../images/fingerprint.png";
 
-export default function Login () {
-  const [user,setUser]=useState({})
+export default function Login() {
+  const [user, setUser] = useState({});
+
   const handleInputChange = (field, value) => {
     setUser({ ...user, [field]: value });
   };
-  const userConnect=(event)=>{
-    event.preventDefault()
-    console.log(user)
-  }
+
+  const userConnect = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
+
   return (
     <div className='formContainer m-0 p-0'>
-      <div className='container-fluid d-flex justify-content-center form '>
-      
-        <form className='col-6 '>
-        <div className='avatar '>
-      </div>
+      <div className='container-fluid d-flex justify-content-center form'>
+        <form className='col-6'>
+          <div className='avatar' style={{ backgroundImage: `url(${userImage})` }}>
+          </div>
           <div className='mb-3'>
-            <label for='emailInput' className='form-label'>
+            <label htmlFor='emailInput' className='form-label'>
               Email address
             </label>
             <input
@@ -28,14 +31,14 @@ export default function Login () {
               id='emailInput'
               aria-describedby='emailHelp'
               placeholder='Enter your email'
-              onChange={(event)=>{handleInputChange('email',event.target.value)}}
+              onChange={(event) => { handleInputChange('email', event.target.value) }}
             />
             <div id='emailHelp' className='form-text'>
               We'll never share your email with anyone else.
             </div>
           </div>
           <div className='mb-3'>
-            <label for='passwordInput' className='form-label'>
+            <label htmlFor='passwordInput' className='form-label'>
               Password
             </label>
             <input
@@ -43,31 +46,30 @@ export default function Login () {
               className='form-control'
               id='passwordInput'
               placeholder='Enter your password'
-              onChange={(event)=>{handleInputChange('password',event.target.value)}}
+              onChange={(event) => { handleInputChange('password', event.target.value) }}
             />
           </div>
           <div className="d-flex justify-content-between">
-             <div className='mb-3 form-check'>
-            <input
-              type='checkbox'
-              className='form-check-input'
-              id='exampleCheck1'
-            />
-            <label className='form-check-label' for='exampleCheck1'>
-              Check me out
-            </label>
+            <div className='mb-3 form-check'>
+              <input
+                type='checkbox'
+                className='form-check-input'
+                id='exampleCheck1'
+              />
+              <label className='form-check-label' htmlFor='exampleCheck1'>
+                Check me out
+              </label>
+            </div>
+            <div className='mb-3 form-check'>
+              <label className='form-check-label fw-semibold specialText' htmlFor=''>
+                Forgot Password ?
+              </label>
+            </div>
           </div>
-          <div className='mb-3 form-check'>
-            <label className='form-check-label fw-semibold specialText' for=''>
-              Forgot Password ?
-            </label>
-          </div>
-          </div>
-         
-          <button type='submit' className='btn btn-primary submitBTN' onClick={(event)=>{userConnect(event)}}>
+          <button type='submit' className='btn btn-primary submitBTN' onClick={(event) => { userConnect(event) }}>
             Sign In
           </button>
-          <p>Not registred yet ? <span className='fw-semibold specialText'> Create account</span> </p>
+          <p>Not registered yet ? <span className='fw-semibold specialText'> Create account</span> </p>
         </form>
       </div>
     </div>
