7529756d395b993451a4c30085d22e00bf68078b
Safouan <safwentmimmi.st@gmail.com>
2024-03-31 15:34:02 +0100

    fix typos && css

--- src/components/landingComponent.js
+++ src/components/landingComponent.js
@@ -10,7 +10,7 @@ function LandingComponent() {
         <div className="container-fluid  landingComponent" id="landing">
             <div className="content container-fluid d-flex flex-column justify-content-center align-items-center h-100 row-gap-5">
                 <p className='slogan '>{t('apply_visa_now', 'Apply For A Visa Now')}</p>
-                <p className='sloganMessage'>{t('visa_with_direct', 'With Direct, you can get travel visas with ease')}</p>
+                <p className='sloganMessage'>{t('visa_with_direct', 'With Fast Service Travel, you can get travel visas with ease')}</p>
                 <NavLink to={"/visaDemand"}>
                     <button className="btn btnApply">{t('apply_now', 'Apply for visa now !')}</button>
                 </NavLink>
